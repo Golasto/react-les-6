f735ba0de0175ed2bcb204cbdd5ee0d803849cf6
Golasto <vdgraafmatteus@gmail.com>
2022-12-23 14:13:26 +0100

    Huiswerk gemaakt

---
+++ src/context/AuthContext.js
@@ -0,0 +1,33 @@
+import { createContext, useState } from "react";
+import { useNavigate } from 'react-router-dom';
+
+export const AutContext = createContext();
+
+export default function AuthApp({ children }) {
+    const [isAuthenticated, setIsAuthenticated] = useState({isAuth: false, username: '', email: ''});
+    const navigate = useNavigate();
+
+    const data = {
+        auth: isAuthenticated,
+        logIn: () => {
+            setIsAuthenticated({isAuth: true, email: document.getElementById("email").value, username: document.getElementById("username").value});
+            console.log('Gebruiker is ingelogd!');
+            navigate('/profile')
+        },
+        logOut: () => {
+            setIsAuthenticated({isAuth: false, user: '', email: ''});
+            console.log('Gebruiker is uitgelogd');
+            navigate('/')
+        }
+    }
+    return (
+        <>
+            <AutContext.Provider value={data}>
+                {children}
+            </AutContext.Provider>
+        </>
+    )
+}
+
+export class AuthContext {
+}
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,22 +1,29 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { AutContext } from '../context/AuthContext';
+import { Link, Navigate } from 'react-router-dom';
 
 function Profile() {
-  return (
-    <>
-      <h1>Profielpagina</h1>
-      <section>
-        <h2>Gegevens</h2>
-        <p><strong>Gebruikersnaam:</strong> hardcoded-test</p>
-        <p><strong>Email:</strong> hardcoded@test.com</p>
-      </section>
-      <section>
-        <h2>Strikt geheime profiel-content</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias cum debitis dolor dolore fuga id molestias qui quo unde?</p>
-      </section>
-      <p>Terug naar de <Link to="/">Homepagina</Link></p>
-    </>
-  );
+    const value = useContext(AutContext);
+
+    if (value.auth.isAuth) {
+        return (
+            <>
+                <h1>Profielpagina</h1>
+                <section>
+                    <h2>Gegevens</h2>
+                    <p><strong>Gebruikersnaam:</strong> {value.auth.username} </p>
+                    <p><strong>Email:</strong> {value.auth.email} </p>
+                </section>
+                <section>
+                    <h2>Strikt geheime profiel-content</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias cum debitis dolor dolore fuga id molestias qui quo unde?</p>
+                </section>
+                <p>Terug naar de <Link to="/">Homepagina</Link></p>
+            </>
+        );
+    }
+
+    return <Navigate to='/signin' replace />
 }
 
 export default Profile;
